ba9bee22d9d9bc95240812f5979f1177693d3206
Ante <antegrbic8@gmail.com>
2013-06-07 15:34:16 +0200

    Update side_ratio.m

--- src/side_ratio.m
+++ src/side_ratio.m
@@ -1,6 +1,6 @@
 function ratio = side_ratio(image)
 %Function that receives image in RGB format. It converges it into Grayscale format and then
-$returns the ratio of its sides.
+%returns the ratio of its sides.
 
 I = rgb2gray(image);
 
