ba4d916378247ef4ce30cd11082540b9c6be3f62
Anto Čabraja <cabraja.anto@gmail.com>
2013-06-07 14:52:50 +0200

    refaktoriranje

--- src/ratio_black_white.m
+++ src/ratio_black_white.m
@@ -1,4 +1,5 @@
-function [feature_value, countBlack, countWhite] = ratio_black_white(A, interval_black, interval_white, precision)
+function [feature_value, countBlack, countWhite] = ...
+    ratio_black_white(A, interval_black, interval_white, precision)
 %Vraca omjer tamnih i svijetlih pixela (crno-bjelo) gleda sve pixele u
 %zadanom intervalu.
 %povratni parametri: feature_value : vrijednost omjera (double)
@@ -18,8 +19,12 @@ function [feature_value, countBlack, countWhite] = ratio_black_white(A, interval
 %        precision = 5*ones(1,3)          /interval sirine 5 oko zadanog
 %        [x, bc, wc] = ratio_black_white(A, interval_black,.....)
 
-    blackPixelMask = abs(A(:,:,1) - interval_black(1)) < precision(1) & abs(A(:,:,2) - interval_black(2)) < precision(2) & abs(A(:,:,3) - interval_black(3)) < precision(3);
-    whitePixelMask = abs(A(:,:,1) - interval_white(1)) < precision(1) & abs(A(:,:,2) - interval_white(2)) < precision(2) & abs(A(:,:,3) - interval_white(3)) < precision(3);
+    blackPixelMask = abs(A(:,:,1) - interval_black(1)) < precision(1) ...
+        & abs(A(:,:,2) - interval_black(2)) < precision(2) ...
+        & abs(A(:,:,3) - interval_black(3)) < precision(3);
+    whitePixelMask = abs(A(:,:,1) - interval_white(1)) < precision(1) ...
+        & abs(A(:,:,2) - interval_white(2)) < precision(2) ...
+        & abs(A(:,:,3) - interval_white(3)) < precision(3);
     
     countBlack = nnz(blackPixelMask);
     countWhite = nnz(whitePixelMask);
