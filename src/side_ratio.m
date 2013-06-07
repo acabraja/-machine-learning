382bb1d513b392ac2dbd4733f6c69550f373eeb7
Ante <antegrbic8@gmail.com>
2013-06-07 15:33:52 +0200

    Create side_ratio.m

---
+++ src/side_ratio.m
@@ -0,0 +1,11 @@
+function ratio = side_ratio(image)
+%Function that receives image in RGB format. It converges it into Grayscale format and then
+$returns the ratio of its sides.
+
+I = rgb2gray(image);
+
+[x,y] = size(I);
+
+ratio = y/x;
+
+end
